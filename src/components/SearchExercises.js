26972364e49c3a99240bf9d77affe95a46eaeded
Faithkebenei <faithkebs16@gmail.com>
2023-06-15 10:17:29 +0300

    Added the body part component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,11 +1,21 @@
 import React, {useEffect, useState} from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import HorizontalScrollbar from './HorizontalScrollbar';
 
-
-const SearchExercises = () => {
+const SearchExercises = ( {setExercises, bodyPart, setBodyPart} ) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([])
+  const [bodyParts, setBodyParts] = useState([])
+
+  useEffect(() => {
+    const fetchExerciseData = async() => {
+      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      
+      setBodyParts(['all', ...bodyPartsData]);
+    }
+    fetchExerciseData();
+  }, [])
+  
 
   const handleSearch = async () => {
     if (search) {
@@ -55,6 +65,10 @@ const SearchExercises = () => {
           Search
         </Button>
       
+      </Box>
+
+      <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
       </Box>
     </Stack>
   )
